c22681d4c89aca6c4c64a0898670ee4957638a6a
CahMoura <Caique.demoura0@gmail.com>
2023-10-22 15:46:56 -0300

    Digital-clock

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const time = setInterval(() => {
+
+    let dateToday = new Date();
+    let hour = dateToday.getHours();
+    let minute = dateToday.getMinutes;
+    let second = dateToday.getSeconds;
+
+    let horas=document.getElementsByClassName("hours").Value='5';
+    document.getElementById('minutes');
+    document.getElementById('seconds');
+
+});
