d9e8bbc4c603056378521fca945c3bc062b2bcfa
CahMoura <Caique.demoura0@gmail.com>
2023-11-10 12:34:48 -0300

    add function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-const time = setInterval(() => {
+const horas = document.getElementById('hours');
+const minutos = document.getElementById('minutes');
+const segundos = document.getElementById('seconds');
+
+const clock = setInterval(function time(){
 
     let dateToday = new Date();
     let hour = dateToday.getHours();
-    let minute = dateToday.getMinutes;
-    let second = dateToday.getSeconds;
+    let minute = dateToday.getMinutes();
+    let second = dateToday.getSeconds();
 
-    let horas=document.getElementsByClassName("hours").Value='5';
-    document.getElementById('minutes');
-    document.getElementById('seconds');
+    if (second < 10)
+        second = '0' + second;
+    if (minute < 10)
+        minute = '0' + minute;
+    if (hour < 10)
+        hour = '0' + hour;
 
-});
+        horas.textContent = hour;
+        minutos.textContent = minute;
+        segundos.textContent = second;
+
+})
